d9afe1fc47c9d53f71c654e307730cf59031a581
Odir123 <Odir123@users.noreply.github.com>
2024-07-29 11:13:08 -0600

    estructura del proyecto y configuracion de layout

---
+++ src/main/java/org/esfe/AlquilerDeCanchaApplication.java
@@ -0,0 +1,13 @@
+package org.esfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerDeCanchaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlquilerDeCanchaApplication.class, args);
+	}
+
+}
