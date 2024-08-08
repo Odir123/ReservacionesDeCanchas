de9753e38982690fdb6bc31eda4ecf9b3f3d3200
Odir123 <Odir123@users.noreply.github.com>
2024-08-08 10:37:36 -0600

    primer commit

--- src/main/java/org/esfe/controlers/HomeController.java
+++ src/main/java/org/esfe/controlers/HomeController.java
@@ -10,4 +10,35 @@ public class HomeController {
     @GetMapping
     public String index(){return "home/index";}
 
-}
+
+
+
+        @GetMapping("/privacy")
+        public String privacy(){
+            return "home/privacy";
+        }
+
+        @GetMapping("/terms")
+        public String terms(){
+            return "home/terms";
+        }
+        @GetMapping("/donde-estamos")
+        public String dondeEstamos() {
+            return "home/donde-estamos"; // Asegúrate de que el archivo se llame donde-estamos.html y esté en la carpeta templates/home
+        }
+
+        @GetMapping("/preguntas-frecuentes")
+        public String preguntasFrecuentes() {
+            return "home/preguntas-frecuentes";
+        }
+
+        @GetMapping("/precios")
+        public String precios() {
+            return "home/precios";
+        }
+
+        @GetMapping("/reservar-cancha")
+        public String reservarCancha() {
+            return "canchas/reservar";
+        }
+    }
